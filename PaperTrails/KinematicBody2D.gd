44eee3ac14158439cbed87bdbec87e060745c3e3
Braden Hearn <Bhearn@stu.jsu.edu>
2018-06-28 19:14:40 -0500

    Scene Change Implemented
    Project up to part 4 of Youtube tutorial

--- PaperTrails/KinematicBody2D.gd
+++ PaperTrails/KinematicBody2D.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 const UP = Vector2(0, -1)
 const GRAVITY = 20
 const SPEED = 200
-const JUMP_HEIGHT = -500
+const JUMP_HEIGHT = -400
 
 var motion = Vector2()
 
@@ -12,14 +12,24 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
+		$Sprite.play("Idle")
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
 	
 	
 	motion = move_and_slide(motion, UP)
